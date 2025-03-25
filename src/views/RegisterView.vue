de4ed63d90369b2a5397095402c8cee7b29c9ed1
ZakaryaMira <bleakdz@gmail.com>
2025-03-25 17:12:06 +0100

    chore: finish the register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,53 @@
 <template>
-  <section class="bg-[#98C1D9]">
-    <h1 class="text-4xl text-black">hello mama</h1>
+  <section class="bg-[#98C1D9] flex items-center justify-center flex-col h-250">
+    <div class="text-white text-5xl font-bold cairo-font text-center">
+      <h1 class="mb-10">صوت</h1>
+    </div>
+    <form class="text-white cairo-font bg-[#6C9CB8] w-125 h-150 p-15 text-xl">
+      <div class="flex flex-col mb-5">
+        <label for="" class="mb-2">الاسم</label>
+        <input
+          class="bg-[#ffffff] w-100 h-10 rounded-md"
+          type="text"
+          placeholder="الرجاء ادخال اسم المستخدم"
+        />
+      </div>
+      <div class="flex flex-col mb-5">
+        <label for="" class="mb-2">اللقب</label>
+        <input
+          type="text"
+          placeholder="الرجاء ادخال لقب المستخدم"
+          class="bg-[#ffffff] w-100 h-10 rounded-md"
+        />
+      </div>
+      <div class="flex flex-col mb-5">
+        <label class="mb-2" for="">البريد الالكتروني</label>
+        <input
+          type="text"
+          placeholder="الرجاء ادخال بريدك الالكتروني "
+          class="bg-[#ffffff] w-100 h-10 rounded-md"
+        />
+      </div>
+      <div class="flex flex-col mb-5">
+        <label class="mb-2" for="">كلمة المرور</label>
+        <input
+          type="text"
+          placeholder="الرجاء ادخال كلمة المرور  "
+          class="bg-[#ffffff] w-100 h-10 rounded-md"
+        />
+      </div>
+      <div class="flex flex-col mb-5">
+        <label class="mb-2" for="">تاكيد كلمة المرور</label>
+        <input
+          type="text"
+          placeholder="الرجاء تاكيد كلمة المرور  "
+          class="bg-[#ffffff] w-100 h-10 rounded-md"
+        />
+      </div>
+      <div class="bg-[#EE6C4D] w-100 h-10 rounded-md text-center p-1">
+        <button>التالي</button>
+      </div>
+    </form>
   </section>
 </template>
 
